3216224c63853e4cf57d142bdc6868aeb2f357d8
Caspar Rinne <caspar@in2exploit.com>
2014-10-15 18:58:36 +0300

    committing input outut page test

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ xrqa
 ====
 
 xrebel test automation demo
+
+Prerequisites: 
+Git, Python 2.7.6, pip, selenium
+
+install pip:
+sudo apt-get install python-pip
+
+install selenium:
+pip install selenium
+
+How to run:
+
+in terminal: python ioPageTests.py
---
+++ ioPageTests.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+import time
+
+base_url = "http://localhost:9966/petclinic/"
+
+driver = webdriver.Firefox()
+driver.implicitly_wait(5)
+driver.get(base_url)
+
+assert "PetClinic :: a Spring Framework demonstration" in driver.title
+elem = driver.find_element_by_link_text("Find owners")
+elem.click()
+
+#searching owners
+elem = driver.find_element_by_class_name("form-actions")
+find_owners_btn = elem.find_elements_by_tag_name("button")
+find_owners_btn[0].click()
+#todo i need to slow down the webdriver to wait table is populated with data.
+#todo change time.sleep() to wait command
+time.sleep(2)
+
+
+#switching to toolbar frame
+driver.switch_to.frame(driver.find_element_by_id("__xrMenuFrame"))
+
+#opening IO page
+elem = driver.find_element_by_id("tool-queries").click()
+
+# switching to IO frame
+driver.switch_to.default_content()
+
+ioFrame = driver.find_element_by_id("__xrIoFrame")
+driver.switch_to.frame(ioFrame)
+
+#searching IO name
+header_elem = driver.find_element_by_tag_name("h1")
+assert "Input / Output" in header_elem.text
+
+#searching close button
+close_btn = driver.find_element_by_id("mainClose")
+close_btn.click()
+assert "Input / Output" not in header_elem.text
+
+driver.close()
+print "\033[0;32mTest Passed!\033[0m"
